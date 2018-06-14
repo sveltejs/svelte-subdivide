e94c0748957fc5363fbca3099a50bf7ed4f91be6
Rich Harris <richard.a.harris@gmail.com>
2018-06-14 11:40:12 -0400

    remove unused constants

--- src/constants.js
+++ src/constants.js
@@ -3,8 +3,5 @@ export const SOUTH = 'SOUTH';
 export const EAST = 'EAST';
 export const WEST = 'WEST';
 
-export const HORIZONTAL = 'HORIZONTAL';
-export const VERTICAL = 'VERTICAL';
-
 export const IS_MAC = navigator.platform === 'MacIntel';
 export const KEYCODE = IS_MAC ? 91 : 17; // Cmd : Ctrl
