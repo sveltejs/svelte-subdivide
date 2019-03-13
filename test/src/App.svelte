b0b99c5224286d0d4c8ca13ba3976a0e56c4f6cf
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2019-03-13 03:49:22 -0300

    Added logic to reset module vars after each test.

--- test/src/App.svelte
+++ test/src/App.svelte
@@ -1,7 +1,10 @@
 <script>
 	import Subdivide from '../../src/Subdivide.svelte';
 	import Item from './Item.svelte';
+	import { resetIndex } from './counter.js';
 
+	resetIndex();
+	
 	export let layout;
 </script>
 
--- test/src/Item.svelte
+++ test/src/Item.svelte
@@ -1,11 +1,9 @@
 <script context='module'>
-    let i = 0;
-    function getIndex() {
-        return i++;
-    }
 </script>
 <script>
-    export let index = getIndex();
+    import { getIndex } from './counter.js';
+
+    let index = getIndex();
 </script>
 
 <span>{index}</span>
