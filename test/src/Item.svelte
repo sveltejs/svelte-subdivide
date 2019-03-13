9953e138d0c9a91bdbf39656c31712242ac91075
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2019-03-13 01:43:45 -0300

    Made index auto-increment on get.

--- test/src/Item.svelte
+++ test/src/Item.svelte
@@ -1,5 +1,11 @@
+<script context='module'>
+    let i = 0;
+    function getIndex() {
+        return i++;
+    }
+</script>
 <script>
-    export let index = 0;
+    export let index = getIndex();
 </script>
 
 <span>{index}</span>
